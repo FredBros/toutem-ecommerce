0b08c65ca3a69fe3695a3a341837d845a29ab682
FredBros <fred.b.devweb@gmail.com>
2022-12-21 15:02:38 +0100

    Minimal NextUi setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '../styles/globals.css'
+import { NextUIProvider } from "@nextui-org/react";
 import type { AppProps } from 'next/app'
+import {theme} from "../styles/theme"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider theme={theme}>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  )
 }
---
+++ styles/theme.jsx
@@ -0,0 +1,13 @@
+import { createTheme, NextUIProvider } from "@nextui-org/react";
+
+
+export const theme = createTheme({
+  type: "dark", // it could be "light" or "dark"
+  theme: {
+    colors: {
+      primary: "#4ADE7B",
+      secondary: "#F9CB80",
+      error: "#FCC5D8",
+    },
+  },
+});
