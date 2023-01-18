1b0429993f62b464afd44daa4e87c0182af84be5
Fred Brossard <fred.b.devweb@gmail.com>
2023-01-18 15:29:18 +0100

    Fix : sanity type alerts

--- toutem-sanity/sanity.config.ts
+++ toutem-sanity/sanity.config.ts
@@ -1,5 +1,8 @@
+//@ts-ignore
 import {defineConfig} from 'sanity'
+//@ts-ignore
 import {deskTool} from 'sanity/desk'
+//@ts-ignore
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
